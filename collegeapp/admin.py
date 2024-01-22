9273d5bf78d5841339b7dcd30ef8d9c58d1a98ef
christybipin <147985640+christybipin@users.noreply.github.com>
2024-01-22 11:21:47 +0530

    first commit

---
+++ collegeapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from collegeapp.models import Country, field, City, Person, login
+
+# Register your models here.
+admin.site.register(field)
+admin.site.register(login)
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Person)
